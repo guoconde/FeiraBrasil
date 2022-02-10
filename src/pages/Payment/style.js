03f6fd808db17e1d402d7fe11920baaeb480c538
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-02-10 14:17:55 -0300

    feat: add step to confirm purchase

--- src/pages/Payment/style.js
+++ src/pages/Payment/style.js
@@ -11,6 +11,7 @@ export const PaymentSection = styled.div`
     padding: 20px 15px;
     background:#fff;
     height:100%;
+    opacity: ${ ({active}) => active ? 1 : .4};
 `
 export const SectionTitle = styled.h2`
     font-size:18px;
@@ -94,4 +95,57 @@ export const ContinueButton = styled.button`
     background:#eee;
     border:0;
     border-radius: 30px;
+`
+export const PaymentForm = styled.div`
+    display:flex;
+    flex-direction:column;
+    h3{
+        font-size:13px;
+        color: #fa5400;
+        padding-bottom: 10px;
+        border-bottom: 1px solid #ccc;
+    }
+`
+export const OptionArea = styled.div`
+    display:flex;
+    margin-bottom:30px;
+`
+export const PaymentOptions = styled.div`
+    display:flex;
+    flex-direction:column;
+    flex:2;
+`
+export const PaymentOption = styled.div`
+    display:flex;
+    flex-direction:column;
+    border:1px solid #ccc;
+    border-top:0;
+    font-size:10px;
+    padding:3px;
+    cursor:pointer;
+`
+
+export const PaymentInfo = styled.div`
+    display:flex;
+    flex-direction:column;
+    padding:15px 0 0 10px;
+    flex:6;
+`
+
+export const CreditCard = styled.div`
+    display:flex;
+    flex-direction:column;
+    gap:10px;
+    padding-top:15px;
+    input{
+        outline:0;
+        padding:3px;
+    }
+`
+export const BankSlip = styled.ul`
+    padding-left: 15px;
+    font-size:12px;
+    display:flex;
+    flex-direction:column;
+    gap:5px;
 `
